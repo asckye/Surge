0d620bedf338c1c7622ccc140e4115c5a499db0c
asckye <63140641+asckye@users.noreply.github.com>
2021-12-12 18:09:52 +0800

    Update and rename startup.js to jd_adblock.js

---
+++ js/jd_adblock.js
@@ -0,0 +1,4 @@
+# 京东
+^https?:\/\/api\.m\.jd\.com\/client\.action\?functionId=start url script-response-body https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/startup/startup.js
+
+hostname = api.m.jd.com,
